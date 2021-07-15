66482e1b8c07e6289287a0a8ba85b221ac427f4b
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2021-07-16 00:28:33 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -16,8 +19,46 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  Random random = new Random();
+  int leftDiceNumber = Random().nextInt(7);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    print('left button pressed');
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    print('Right button pressed');
+                    Tooltip(
+                      message: 'right button pressed',
+                    );
+                  },
+                  child: Image.asset(
+                    'images/dice2.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
